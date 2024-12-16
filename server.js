a8a4757123e1a3f576cff5e9420a4a326d633710
EmilyBirschner <emilyb.oliveira@hotmail.com>
2024-12-16 17:06:58 -0300

    teste

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000; // Use a porta fornecida pelo servidor
+const PORT = process.env.PORT || 3000; // Usa a porta fornecida pela KingHost
 
 // Servir arquivos estáticos da pasta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
