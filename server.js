0d49d5787620e67c06b3afd2947efe33a284cf46
EmilyBirschner <emilyb.oliveira@hotmail.com>
2024-12-16 17:46:53 -0300

    agora vai

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000; // Usa a porta fornecida pela KingHost
 
 // Servir arquivos estáticos da pasta 'public'
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, ['public'])));
 
 // Rota principal
 app.get('/', (req, res) => {
