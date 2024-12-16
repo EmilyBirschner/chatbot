28af7440542f1accb615d749a764476369564361
EmilyBirschner <emilyb.oliveira@hotmail.com>
2024-12-16 17:09:28 -0300

    nome

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Rota principal
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'chatbot.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Inicializa o servidor
