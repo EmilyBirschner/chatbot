f4879c2f36333e56ce3cde1a44642b6c246d53a3
EmilyBirschner <emilyb.oliveira@hotmail.com>
2024-12-16 16:35:09 -0300

    ajuste

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ io.on('connection', (socket) => {
 });
 
 // Configuração da porta do servidor
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
